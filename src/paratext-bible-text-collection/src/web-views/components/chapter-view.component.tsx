001d415cf2166ad446796847fb290fae84e95580
tjcouch-sil <tj_couch@sil.org>
2024-03-06 17:14:38 -0600

    Fixed some CSS issues

--- src/paratext-bible-text-collection/src/web-views/components/chapter-view.component.tsx
+++ src/paratext-bible-text-collection/src/web-views/components/chapter-view.component.tsx
@@ -1,9 +1,8 @@
-import { useProjectData } from '@papi/frontend/react';
 import { VerseRef } from '@sillsdev/scripture';
 import { ProjectMetadata } from '@papi/core';
-import { useEffect, useState } from 'react';
 import { logger } from '@papi/frontend';
-import { Editor, Usj, usxStringToJson } from '@biblionexus-foundation/platform-editor';
+import { Editor } from '@biblionexus-foundation/platform-editor';
+import useProjectUsj from '../hooks/use-project-usj.hook';
 
 export type ChapterViewProps = {
   projectId: string;
@@ -12,26 +11,14 @@ export type ChapterViewProps = {
 };
 
 function ChapterView({ projectId, projectMetadata, verseRef }: ChapterViewProps) {
-  const [usx] = useProjectData('ParatextStandard', projectId).ChapterUSX(verseRef, '');
+  const [usj] = useProjectUsj(projectId, verseRef);
 
-  const [usj, setUsj] = useState<Usj>();
-  useEffect(() => {
-    if (usx) {
-      try {
-        setUsj(usxStringToJson(usx));
-      } catch (e) {
-        logger.warn(`ResourceViewer convert error: ${e}`);
-      }
-    }
-  }, [usx]);
   return (
     <div className="full-chapter-view">
       <div className="position-title">
         <p>{projectMetadata?.name || '...'}</p>
       </div>
-      <p className="position-text">
-        <Editor usj={usj} scrRef={verseRef} logger={logger} isReadonly />
-      </p>
+      <Editor usj={usj} scrRef={verseRef} logger={logger} isReadonly />
     </div>
   );
 }
