43204261c7e3f12dbd902e4bb766017a2ae99177
Ira Hopkinson <irahopkinson@users.noreply.github.com>
2024-04-09 07:00:09 +1200

    update editor (#14)
    - uncontrolled editor - BREAKING CHANGE
    - reorganize USJ node CSS - BREAKING CHANGE
    - also add `npm run stop`

--- src/paratext-bible-text-collection/src/web-views/hooks/use-project-usj.hook.tsx
+++ src/paratext-bible-text-collection/src/web-views/hooks/use-project-usj.hook.tsx
@@ -1,24 +1,17 @@
 import { VerseRef } from '@sillsdev/scripture';
 import { useProjectData } from '@papi/frontend/react';
 import { useEffect, useState } from 'react';
-import { Usj, usxStringToJson } from '@biblionexus-foundation/platform-editor';
-import { logger } from '@papi/frontend';
+import { Usj, usxStringToUsj } from '@biblionexus-foundation/platform-editor';
 
 export default function useProjectUsj(
   projectId: string,
   verseRef: VerseRef,
 ): [Usj, (usj: Usj) => void] {
   const [usx] = useProjectData('ParatextStandard', projectId).ChapterUSX(verseRef, '');
-
   const [usj, setUsj] = useState<Usj>();
+
   useEffect(() => {
-    if (usx) {
-      try {
-        setUsj(usxStringToJson(usx));
-      } catch (e) {
-        logger.warn(`Usj convert error: ${e}`);
-      }
-    }
+    if (usx) setUsj(usxStringToUsj(usx));
   }, [usx]);
 
   return [usj, setUsj];
