4b2a7750483808eb6e3341e33b72ab1beff0f558
TJ Couch <104016682+tjcouch-sil@users.noreply.github.com>
2025-08-22 14:55:42 -0500

    PT-3107: Fixed problem autoformatting readme (#62)
    Fixed problem autoformatting readme

--- lib/git.util.ts
+++ lib/git.util.ts
@@ -319,9 +319,12 @@ export async function formatExtensionFolder(extensionFolderPath: string) {
     const modifiedTitle = titleSection.map((line) =>
       line.replace(/paranext-extension-template/g, extensionName),
     );
-    const modifiedSummary = summarySection.map((line) =>
-      line.replace(/paranext-extension-template/g, extensionName),
-    );
+    const modifiedSummary = summarySection.map((line) => {
+      if (line.includes('https://github.com/paranext/paranext-extension-template/wiki'))
+        return line;
+
+      return line.replace(/paranext-extension-template/g, extensionName);
+    });
 
     // Reconstruct the README
     const finalLines = [...modifiedTitle, ...betweenTitleAndSummary, ...modifiedSummary, ...after];
